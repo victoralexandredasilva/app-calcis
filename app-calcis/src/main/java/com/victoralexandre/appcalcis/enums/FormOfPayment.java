ca46b3a1f343dffc00612bf8e278f0aac69fddc1
Victor Alexandre <124846412+victoralexandredasilva@users.noreply.github.com>
2024-05-07 23:09:41 -0300

    Delete app-calcis directory

--- app-calcis/src/main/java/com/victoralexandre/appcalcis/enums/FormOfPayment.java
+++
@@ -1,9 +0,0 @@
-package com.victoralexandre.appcalcis.enums;
-
-public enum FormOfPayment {
-    DINHEIRO,
-    PIX,
-    DEBITO,
-    CREDITO,
-    PARCELADO
-}
--- app-calcis/src/main/java/com/victoralexandre/appcalcis/repositories/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.victoralexandre.appcalcis.repositories;
-
-import com.victoralexandre.appcalcis.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Category findFirstByName(String name);
-}
